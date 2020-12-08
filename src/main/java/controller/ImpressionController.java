bc872b28b155b276faaeba2c7a63d3745fa1d83e
bulan_zade <631343306@qq.com>
2020-12-08 11:54:38 +0800

    add ImpressionController.java and update web.xml

---
+++ src/main/java/controller/ImpressionController.java
@@ -0,0 +1,19 @@
+/************************
+     Author: 丁宇翔
+     Date: 2020.12.7
+ ************************/
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ImpressionController extends HttpServlet
+{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+
+    }
+}
